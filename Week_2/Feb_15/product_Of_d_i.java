05e7082021020860fac56ccf341a5afc83c08296
shailesh H D <59162665+shaileshhd@users.noreply.github.com>
2022-02-20 16:59:37 +0530

    Feb_15 Assignments

---
+++ Week_2/Feb_15/product_Of_d_i.java
@@ -0,0 +1,11 @@
+package com.Feb_15;
+
+public class product_Of_d_i {
+	public static void main(String[] args) {
+		float a = 8.2f;
+		int b = 6;
+		float productFloat = a * b;
+		int productInt = (int) a * b; // Type_Conversion
+		System.out.println(productFloat + " " + productInt);
+	}
+}
---
+++ Week_2/Feb_15/value_ASCII.java
@@ -0,0 +1,13 @@
+package com.Feb_15;
+
+import java.util.*;
+
+public class value_ASCII {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char c = s.next().charAt(0);
+		int i = c;
+		System.out.println(i);
+		s.close();
+	}
+}
